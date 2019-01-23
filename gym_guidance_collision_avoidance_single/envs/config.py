f2895b59d7ce54c4cd75245f17c2b8c4bafe1dfd
xuxiyang <xuxiyang1993@gmail.com>
2019-01-22 23:38:23 -0600

    Update config.py

--- gym_guidance_collision_avoidance_single/envs/config.py
+++ gym_guidance_collision_avoidance_single/envs/config.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Config:
     # input dim
     window_width = 800
@@ -11,18 +12,18 @@ class Config:
     scale = 30
 
     # distance param
-    minimum_separation = 555/scale
-    NMAC_dist = 150/scale
-    horizon_dist = 4000/scale
-    initial_min_dist = 3000/scale
-    goal_radius = 600/scale
+    minimum_separation = 555 / scale
+    NMAC_dist = 150 / scale
+    horizon_dist = 4000 / scale
+    initial_min_dist = 3000 / scale
+    goal_radius = 600 / scale
 
     # speed
-    min_speed = 50/scale
-    max_speed = 80/scale
-    d_speed = 5/scale
-    speed_sigma = 2/scale
-    position_sigma = 10/scale
+    min_speed = 50 / scale
+    max_speed = 80 / scale
+    d_speed = 5 / scale
+    speed_sigma = 2 / scale
+    position_sigma = 10 / scale
 
     # heading in deg TBD
     d_heading = math.radians(5)

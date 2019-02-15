d4d247c7f7c520d703219b18a5b0cec14c5a2a9b
xuxiyang1993 <xuxiyang1993@gmail.com>
2019-02-15 13:09:38 -0600

    Update pytorch_dqn.py

--- pytorch/pytorch_dqn.py
+++ pytorch/pytorch_dqn.py
@@ -18,15 +18,19 @@ def train(n_episodes=15000, eps_start=1.0, eps_end=0.01, decay=0.995):
     epsilon = eps_start
     last_mean_reward = -np.inf # compare with reward_window to check save model or not
     for i in range(1, n_episodes+1):
+        episode_timestep = 0
         state = env.reset()
         total_reward = 0
         done = False
         while not done:
+            episode_timestep += 1
             action = agent.act(state, epsilon)
             next_state, reward, done, _ = env.step(action)
             agent.step(state, action, reward, next_state, done)
             state = next_state
             total_reward += reward
+            if episode_timestep > 1000:
+                break
         reward_window.append(total_reward)
         total_rewards.append(total_reward)
         epsilon = max(eps_end, decay*epsilon)

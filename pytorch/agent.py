48d34cc7007b8233cabe9f1c971852d9b13e35e0
xuxiyang1993 <xuxiyang1993@gmail.com>
2019-02-20 16:20:33 -0600

    Update args

--- pytorch/agent.py
+++ pytorch/agent.py
@@ -13,29 +13,31 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 BUFFER_SIZE = int(1e5)
 BATCH_SIZE = 64
 GAMMA = 0.99
-TAU = 1e-3 # parameter for soft update
+TAU = 1e-3  # parameter for soft update
 LEARNING_RATE = 1e-4
 UPDATE_EVERY = 5
 
+
 class Agent():
     '''Agent interacts with the environment'''
-    
-    def __init__(self, state_size, action_size):
+
+    def __init__(self, state_size, action_size, HER=False):
         self.state_size = state_size
         self.action_size = action_size
-        
+        self.HER = HER
+
         # double network
         self.local = QNetwork(state_size, action_size).to(device)
         # move model to either gpu or cpu
         self.target = QNetwork(state_size, action_size).to(device)
         self.optimizer = optim.Adam(self.local.parameters(), lr=LEARNING_RATE)
-        
+
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE)
         self.time_step = 0
-        
+
     def step(self):
         # self.memory.add(state, action, reward, next_state, done)
-        
+
         # Learn every UPDATE_EVERY time steps.
         self.time_step = (self.time_step + 1) % UPDATE_EVERY
         if self.time_step == 0:
@@ -43,20 +45,20 @@ class Agent():
             if len(self.memory.memory) > BATCH_SIZE:
                 experiences = self.memory.sample()
                 self.learn(experiences, GAMMA)
-                
+
     def act(self, state, epsilon=0.):
         '''Choose an action given state using epsilon-greedy'''
         state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        self.local.eval() # change model to evaluation mode
-        with torch.no_grad(): # turn off gradient descent since evaluating
+        self.local.eval()  # change model to evaluation mode
+        with torch.no_grad():  # turn off gradient descent since evaluating
             q_value = self.local(state)
-        self.local.train() # back to train mode
-        
+        self.local.train()  # back to train mode
+
         if random.random() > epsilon:
-            return np.argmax(q_value.cpu().data.numpy()) # move q_value to cpu
+            return np.argmax(q_value.cpu().data.numpy())  # move q_value to cpu
         else:
             return random.choice(np.arange(self.action_size))
-        
+
     def learn(self, experiences, gamma):
         '''learning from batch'''
         states, actions, rewards, next_states, dones = experiences
@@ -66,23 +68,22 @@ class Agent():
         q_target = rewards + (gamma * q_target_next * (1 - dones))
         # if episode is done q_target equals to reward only
         q_expected = self.local(states).gather(1, actions)
-        
+
         # gradient step
         loss = F.mse_loss(q_expected, q_target)
-        self.optimizer.zero_grad() # zero gradient if not pytorch will accmulate
+        self.optimizer.zero_grad()  # zero gradient if not pytorch will accmulate
         loss.backward()
         self.optimizer.step()
-        
+
         self.soft_update(self.local, self.target, TAU)
-        
+
     def soft_update(self, local_net, target_net, tau):
         '''soft update for parameters
         θ_target = tau*θ_local + (1 - tau)*θ_target
         '''
         for target_param, local_param in zip(target_net.parameters(), local_net.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
-        
-        
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
     def add(self, episode_experience, env):
         '''
         HER add experience after episodes terminates
@@ -92,22 +93,24 @@ class Agent():
             inputs = np.concatenate([s, g], axis=-1)
             new_inputs = np.concatenate([s_n, g], axis=-1)
             self.memory.add(inputs, a, r, new_inputs, done)
-            
-            for k in range(4):
-                future = np.random.randint(t, len(episode_experience))
-                _, _, _, g_n, _, _ = episode_experience[future]
-                inputs = np.concatenate([s, g_n[:2]], axis = -1)
-                new_inputs = np.concatenate([s_n, g_n[:2]], axis=-1)
-                r_n = env.compute_reward(s_n[:2], g_n[:2], _)
-                if r_n == 1:
-                    done = True
-                else:
-                    done = False
-                self.memory.add(inputs, a, r_n, new_inputs, done)
-                
+
+            if self.HER:
+                for k in range(4):
+                    future = np.random.randint(t, len(episode_experience))
+                    _, _, _, g_n, _, _ = episode_experience[future]
+                    inputs = np.concatenate([s, g_n[:2]], axis=-1)
+                    new_inputs = np.concatenate([s_n, g_n[:2]], axis=-1)
+                    r_n = env.compute_reward(s_n[:2], g_n[:2], _)
+                    if r_n == 1:
+                        done = True
+                    else:
+                        done = False
+                    self.memory.add(inputs, a, r_n, new_inputs, done)
+
+
 class ReplayBuffer():
     '''Buffer stores experience'''
-    
+
     def __init__(self, action_size, buffer_size, batch_size):
         self.memory = deque(maxlen=buffer_size)
         self.action_size = action_size
@@ -115,22 +118,24 @@ class ReplayBuffer():
         self.experience = namedtuple('experience', field_names=['state', 'action',
                                                                 'reward', 'next_state',
                                                                 'done'])
+
     def add(self, state, action, reward, next_state, done):
         '''add new memory'''
         memory = self.experience(state, action, reward, next_state, done)
         self.memory.append(memory)
-        
+
     def sample(self):
         '''random sample a batch'''
-        experiences = random.sample(self.memory, k=self.batch_size)# return a list of experience
-        
+        experiences = random.sample(self.memory, k=self.batch_size)  # return a list of experience
+
         # restack the list of experience into batch form
         # np.vstack stacks lists verticallly
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-        
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
+            device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
+            device)
+
         return (states, actions, rewards, next_states, dones)
-        
--- pytorch/dqn_her.py
+++ pytorch/dqn_her.py
@@ -7,19 +7,20 @@ from SingleAircraftHEREnv import SingleAircraftHEREnv
 from agent import Agent
 
 
-def train(env, agent, n_episodes=30000, eps_start=1.0, eps_end=0.01, decay=0.9999):
+def train(env, agent, n_episodes=30000, eps_start=1.0, eps_end=0.01, decay=0.9999,
+          save_path='save_model\checkpoint.pth'):
     # training loop
     total_rewards = []
     reward_window = deque(maxlen=100)
     epsilon = eps_start
-    last_mean_reward = -np.inf # compare with reward_window to check save model or not
-    for i in range(1, n_episodes+1):
+    last_mean_reward = -np.inf  # compare with reward_window to check save model or not
+    for i in range(1, n_episodes + 1):
         episode_timestep = 0
         last_ob = env.reset()
-        episode_experience = [] # experience in one episode
+        episode_experience = []  # experience in one episode
         total_reward = 0
         done = False
-        
+
         while not done:
             # env.render()
             observation = np.copy(last_ob['observation'])
@@ -35,19 +36,20 @@ def train(env, agent, n_episodes=30000, eps_start=1.0, eps_end=0.01, decay=0.999
             total_reward += reward
             if episode_timestep > 1000:
                 break
-        
+
         agent.add(episode_experience, env)
-        
+
         reward_window.append(total_reward)
         total_rewards.append(total_reward)
-        epsilon = max(eps_end, decay*epsilon)
+        epsilon = max(eps_end, decay * epsilon)
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i, np.mean(reward_window)), end="")
         if i % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i, np.mean(reward_window)))
         if i >= 1000:
             if last_mean_reward < np.mean(reward_window) or i % 100 == 0:
-                torch.save(agent.local.state_dict(), 'checkpoint.pth')
-                print('\rEpisode {}\tAverage Score: {:.2f}\tPrevious Score: {:.2f}'.format(i, np.mean(reward_window), last_mean_reward))
+                torch.save(agent.local.state_dict(), save_path)
+                print('\rEpisode {}\tAverage Score: {:.2f}\tPrevious Score: {:.2f}'.format(i, np.mean(reward_window),
+                                                                                           last_mean_reward))
                 print('Model saved')
                 last_mean_reward = np.mean(reward_window)
 
@@ -64,7 +66,8 @@ def evaluate(env, agent):
     for i in range(10):
         last_ob = env.reset()
         done = False
-        while not done:
+        episode_timestep = 0
+        while not done and episode_timestep < 1000:
             state = last_ob['observation']
             goal = last_ob['desired_goal']
             inputs = np.concatenate([state, goal], axis=-1)
@@ -72,6 +75,8 @@ def evaluate(env, agent):
             env.render()
             ob, reward, done, _ = env.step(action)
             last_ob = ob
+
+            episode_timestep += 1
 
     env.close()
 
@@ -83,13 +88,14 @@ def main():
     parser.add_argument('--play', '-p', type=bool, default=False)
     parser.add_argument('--HER', type=bool, default=False)
     parser.add_argument('--seed', type=int, default=101)
+    parser.add_argument('--save_path', '-s', type=str, default='save_model/checkpoint.pth')
     args = parser.parse_args()
 
     env = SingleAircraftHEREnv()
-    agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.n)
+    agent = Agent(state_size=env.observation_space.shape[0], action_size=env.action_space.n, HER=args.HER)
 
     if not args.play:
-        train(env, agent, n_episodes=args.episodes)
+        train(env, agent, n_episodes=args.episodes, save_path=args.save_path)
 
     evaluate(env, agent)
 
